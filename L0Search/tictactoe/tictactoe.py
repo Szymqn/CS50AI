5dad2fd755fefa336cd1c8b63327aedec3b18b52
szymqn <koodziejskisz@outlook.com>
2023-07-26 18:40:05 +0200

    implement game function

--- L0Search/tictactoe/tictactoe.py
+++ L0Search/tictactoe/tictactoe.py
@@ -22,46 +22,136 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    num_X = sum(row.count(X) for row in board)
+    num_O = sum(row.count(O) for row in board)
+  
+    return O if num_X > num_O else X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_actions = set()
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == EMPTY:
+                possible_actions.add((i, j))
+
+    return possible_actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    i, j = action
+    if board[i][j] != EMPTY:
+        raise ValueError("Invalid action. Cell already occupied")
+
+    new_board = [row[:] for row in board]
+    new_board[i][j] = player(board)
+    return new_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    # Check rows
+    for row in board:
+        if row.count(X) == 3:
+            return X
+        elif row.count(O) == 3:
+            return O
+
+    # Check columns
+    for j in range(3):
+        if board[0][j] == board[1][j] == board[2][j] == X:
+            return X
+        elif board[0][j] == board[1][j] == board[2][j] == O:
+            return O
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] == X or board[0][2] == board[1][1] == board[2][0] == X:
+        return X
+    elif board[0][0] == board[1][1] == board[2][2] == O or board[0][2] == board[1][1] == board[2][0] == O:
+        return O
+
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    return winner(board) is not None or all(all(cell != EMPTY for cell in row) for row in board)
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    result = winner(board)
+    if result == X:
+        return 1
+    elif result == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+
+    current_player = player(board)
+
+    if current_player == X:
+        max_value = -math.inf
+        best_action = None
+
+        for action in actions(board):
+            min_value = min_value_fn(result(board, action))
+            if min_value > max_value:
+                max_value = min_value
+                best_action = action
+
+        return best_action
+
+    else:
+        min_value = math.inf
+        best_action = None
+
+        for action in actions(board):
+            max_value = max_value_fn(result(board, action))
+            if max_value < min_value:
+                min_value = max_value
+                best_action = action
+
+        return best_action
+
+
+def max_value_fn(board):
+    if terminal(board):
+        return utility(board)
+
+    v = -math.inf
+    for action in actions(board):
+        v = max(v, min_value_fn(result(board, action)))
+
+    return v
+
+
+def min_value_fn(board):
+    if terminal(board):
+        return utility(board)
+
+    v = math.inf
+    for action in actions(board):
+        v = min(v, max_value_fn(result(board, action)))
+
+    return v
